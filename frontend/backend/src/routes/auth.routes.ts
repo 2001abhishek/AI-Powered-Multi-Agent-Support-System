5072ea5460c6677ab7deccb32f05f54d3c2cae7d
2001abhishek <rayabhishek9438@gmail.com>
2026-02-11 16:31:10 +0530

    updated

--- frontend/backend/src/routes/auth.routes.ts
+++
@@ -1,8 +0,0 @@
-
-import { Hono } from "hono";
-import { authController } from "../controllers/auth.controller";
-
-export const authRouter = new Hono();
-
-authRouter.post("/login", authController.login);
-authRouter.post("/signup", authController.signup);
